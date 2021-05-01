237772476f169342d84ff312ecbf7d45f6cb05f0
Muhammad Umar Farooq <develop.umar@gmail.com>
2021-05-01 11:05:11 +0500

    set specific date so its not changed

--- src/constants/tasksHardcodedData.ts
+++ src/constants/tasksHardcodedData.ts
@@ -3,31 +3,31 @@ export const defaultTasksData: TaskInterface[] = [
     id: "1",
     title: "This is some title",
     state: "TASK_INBOX",
-    updatedAt: new Date(),
+    updatedAt: new Date(1995, 11, 17, 3, 24, 0),
   },
   {
     id: "2",
     title: "This is title two",
     state: "TASK_INBOX",
-    updatedAt: new Date(),
+    updatedAt: new Date(1995, 11, 17, 3, 24, 0),
   },
   {
     id: "3",
     title: "This is title three",
     state: "TASK_INBOX",
-    updatedAt: new Date(),
+    updatedAt: new Date(1995, 11, 17, 3, 24, 0),
   },
   {
     id: "4",
     title: "This is title four",
     state: "TASK_INBOX",
-    updatedAt: new Date(),
+    updatedAt: new Date(1995, 11, 17, 3, 24, 0),
   },
   {
     id: "5",
     title: "This is title five",
     state: "TASK_INBOX",
-    updatedAt: new Date(),
+    updatedAt: new Date(1995, 11, 17, 3, 24, 0),
   }
 ];
 
@@ -36,32 +36,32 @@ export const pinnedTasksData: TaskInterface[] = [
     id: "1",
     title: "This is some title",
     state: "TASK_PINNED",
-    updatedAt: new Date(),
+    updatedAt: new Date(1995, 11, 17, 3, 24, 0),
   },
   {
     id: "2",
     title: "This is title two",
     state: "TASK_PINNED",
-    updatedAt: new Date(),
+    updatedAt: new Date(1995, 11, 17, 3, 24, 0),
   },
   
   {
     id: "3",
     title: "This is title three",
     state: "TASK_PINNED",
-    updatedAt: new Date(),
+    updatedAt: new Date(1995, 11, 17, 3, 24, 0),
   },
   {
     id: "4",
     title: "This is title four",
     state: "TASK_PINNED",
-    updatedAt: new Date(),
+    updatedAt: new Date(1995, 11, 17, 3, 24, 0),
   },
   {
     id: "5",
     title: "This is title five",
     state: "TASK_PINNED",
-    updatedAt: new Date(),
+    updatedAt: new Date(1995, 11, 17, 3, 24, 0),
   }
 ];
 
@@ -70,31 +70,31 @@ export const archivedTasksData: TaskInterface[] = [
     id: "1",
     title: "This is some title",
     state: "TASK_ARCHIVED",
-    updatedAt: new Date(),
+    updatedAt: new Date(1995, 11, 17, 3, 24, 0),
   },
   {
     id: "2",
     title: "This is title two",
     state: "TASK_ARCHIVED",
-    updatedAt: new Date(),
+    updatedAt: new Date(1995, 11, 17, 3, 24, 0),
   },
   
   {
     id: "3",
     title: "This is title three",
     state: "TASK_ARCHIVED",
-    updatedAt: new Date(),
+    updatedAt: new Date(1995, 11, 17, 3, 24, 0),
   },
   {
     id: "4",
     title: "This is title four",
     state: "TASK_ARCHIVED",
-    updatedAt: new Date(),
+    updatedAt: new Date(1995, 11, 17, 3, 24, 0),
   },
   {
     id: "5",
     title: "This is title five",
     state: "TASK_ARCHIVED",
-    updatedAt: new Date(),
+    updatedAt: new Date(1995, 11, 17, 3, 24, 0),
   }
 ];
